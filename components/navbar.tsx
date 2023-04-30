88f6cb13fdeaaaee1a0f1c16dd139d773db82f2e
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-04-30 01:53:56 -0300

    fix: project name redirec

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ThemeButton from './theme-button';
 import AuthButton from './auth-button';
+import Link from 'next/link';
 
 const Navbar = async () => {
     return (
         <div className='flex flex-row w-full border-b'>
             <div className='flex flex-row justify-between items-center container p-4'>
-                <h1>Project B</h1>
+                <Link href='/' className='font-black'>Project B</Link>
                 <div className='flex flex-row space-x-2'>
                     <ThemeButton />
                     {/* @ts-expect-error Server Component */}
