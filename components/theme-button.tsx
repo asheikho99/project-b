e4933276d2a62839097e691fce26f9f8096f2699
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-05-24 16:47:47 -0300

    feat: Theme Button loading skeleton

--- components/theme-button.tsx
+++ components/theme-button.tsx
@@ -4,11 +4,12 @@ import React, { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 import { Moon, Sun } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const ThemeButton = () => {
   const { systemTheme, theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
-  const [icon, setIcon] = useState(<></>);
+  const [icon, setIcon] = useState(<Skeleton className="h-10 w-14" />);
 
   useEffect(() => {
     setMounted(true);
