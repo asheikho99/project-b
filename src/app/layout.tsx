825432596279413731add10e918a72f3defa672b
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-07-27 02:39:23 -0300

    basic site-nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NextThemeProvider from "@/components/next-theme-provider";
+import SiteFooter from "@/components/site-footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <NextThemeProvider attribute="class" defaultTheme="dark" enableSystem={true}>
           <SiteNav />
           {children}
+          <SiteFooter />
         </NextThemeProvider>
       </body>
     </html>
---
+++ src/components/site-footer.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+
+const SiteFooter = () => (
+  <footer className="border-t">
+    <div className="container mx-auto flex flex-row items-center justify-center space-x-4 py-8">
+      <Link href="/about" className="text-primary/60 hover:text-primary">
+        About this project
+      </Link>
+      <Link
+        href="https://github.com/asheikho99/project-b"
+        className="text-primary/60 hover:text-primary"
+      >
+        {" "}
+        View source code
+      </Link>
+    </div>
+  </footer>
+);
+
+export default SiteFooter;
